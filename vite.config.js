15d34205be52fbad7a449bc3999e89836a61356c
Yogesh Saini <yogeshsaini1213@gmail.com>
2023-11-26 22:22:29 +0530

    Offline System

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    manifest: {
+      theme_color: "#3367D6",
+      icons: [
+        {
+          src: "/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "any maskable"
+        },
+        {
+          src: "/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable"
+        }
+      ],
+      name: 'Physics World',
+      short_name: 'Physics World',
+    
+    },
+    registerType: 'autoUpdate',
+    workbox: {
+      clientsClaim: true,
+      skipWaiting: true
+    },
+    devOptions: {
+      enabled: true
+    }
+  })],
 })

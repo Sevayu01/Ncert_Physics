443e4e2092e1db4af55be15c99030792a734596c
itsspriyansh <priyanshkp01@gmail.com>
2023-11-28 23:41:26 +0530

    reduced prop drill depth in quiz side navigation

--- src/pages/quiz/QuickNavigationButton.jsx
+++ src/pages/quiz/QuickNavigationButton.jsx
@@ -1,20 +1,18 @@
 const QuickNavigationButton = ({ idx, selected }) => {
+  const defaultStyle =
+    "w-16 h-16 rounded-full border-[5px] border-white bg-white text-center text-xl flex justify-center items-center";
+  const correctStyle =
+    "w-16 h-16 rounded-full border-[5px] border-white bg-green-500 text-white font-bold text-center text-xl flex justify-center items-center";
+  const incorrectStyle =
+    "w-16 h-16 rounded-full border-[5px] border-white bg-red-500 text-white font-bold text-center text-xl flex justify-center items-center";
 
-    const defaultStyle = "w-16 h-16 rounded-full bg-white text-center text-xl flex justify-center items-center";
-    const correctStyle = "w-16 h-16 rounded-full bg-[#2caf00] text-center text-xl flex justify-center items-center";
-    const incorrectStyle = "w-16 h-16 rounded-full bg-[#a42929] text-center text-xl flex justify-center items-center";
+  const getStyle = () => {
+    if (selected[idx] === -1) return defaultStyle;
+    if (selected[idx] === 1) return correctStyle;
+    if (selected[idx] === 0) return incorrectStyle;
+  };
 
-    const getStyle = () => {
-        if (selected[idx] === -1) return defaultStyle;
-        if (selected[idx] === 1) return correctStyle;
-        if (selected[idx] === 0) return incorrectStyle; 
-    }
-
-  return (
-    <div className={getStyle()}>
-      {idx+1}
-    </div>
-  );
+  return <div className={getStyle()}>{idx + 1}</div>;
 };
 
 export default QuickNavigationButton;

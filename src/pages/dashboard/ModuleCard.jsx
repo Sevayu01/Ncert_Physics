a7ed2f7a09e9a1944ee377a6b50738697b3d57ad
itsspriyansh <priyanshkp01@gmail.com>
2024-04-13 20:57:31 +0530

    added images for chapter 3

--- src/pages/dashboard/ModuleCard.jsx
+++ src/pages/dashboard/ModuleCard.jsx
@@ -1,4 +1,4 @@
-import quizIcon from "../../assets/quiz.png";
+import quizIcon from "/assets/quiz.png";
 import faqIcon from "/assets/faq.png";
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
